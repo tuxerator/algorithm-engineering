5fdabf5f66dfa05ab00e88bb46f2f856ee959c21
Fiona Riesterer <fiona.riesterer@web.de>
2022-12-21 14:46:56 +0100

    added euclidian distance

--- exercise04/source/src/main/java/ae/sanowskiriesterer/NearestNeighbor.java
+++ exercise04/source/src/main/java/ae/sanowskiriesterer/NearestNeighbor.java
@@ -8,4 +8,9 @@ public class NearestNeighbor {
     
     //find node wit nearest euclidian distance, don't calcualte for every node, try and find possible candidates by using the coordinates
     //ex.: Finding nearest neighbor for Node(0,0) then Node (10,10) cant be nearer than Node(9,9) or Node (10,9) etc.
+
+    public static float euclidianDistance(Node a, Node b){
+
+        return (float) Math.sqrt(Math.pow(a.xCoord-b.xCoord,2) + Math.pow(a.yCoord-b.yCoord,2));
+    }
 }
